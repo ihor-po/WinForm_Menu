aa5f50c57d385c4c1688ce9ce520c77652a09b7d
Ihor Porotikov <sobikashi@gmail.com>
2018-04-28 22:40:15 +0300

    add task3

---
+++ task3/AddMenuItem/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace AddMenuItem
+{
+    partial class cmi_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mci_menu = new System.Windows.Forms.MenuStrip();
+            this.cmi_b_createItem = new System.Windows.Forms.Button();
+            this.cmi_b_createSubItem = new System.Windows.Forms.Button();
+            this.cmi_tb_topItem = new System.Windows.Forms.TextBox();
+            this.cmi_tb_subItem = new System.Windows.Forms.TextBox();
+            this.cmi_l_topItem = new System.Windows.Forms.Label();
+            this.cmi_subItem = new System.Windows.Forms.Label();
+            this.cmi_l_currentItem = new System.Windows.Forms.Label();
+            this.cmi_cb_menuItems = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // mci_menu
+            // 
+            this.mci_menu.Location = new System.Drawing.Point(0, 0);
+            this.mci_menu.Name = "mci_menu";
+            this.mci_menu.Size = new System.Drawing.Size(566, 24);
+            this.mci_menu.TabIndex = 0;
+            this.mci_menu.Text = "menuStrip1";
+            // 
+            // cmi_b_createItem
+            // 
+            this.cmi_b_createItem.Location = new System.Drawing.Point(403, 74);
+            this.cmi_b_createItem.Name = "cmi_b_createItem";
+            this.cmi_b_createItem.Size = new System.Drawing.Size(152, 23);
+            this.cmi_b_createItem.TabIndex = 1;
+            this.cmi_b_createItem.Text = "Создать пункт меню";
+            this.cmi_b_createItem.UseVisualStyleBackColor = true;
+            // 
+            // cmi_b_createSubItem
+            // 
+            this.cmi_b_createSubItem.Location = new System.Drawing.Point(402, 110);
+            this.cmi_b_createSubItem.Name = "cmi_b_createSubItem";
+            this.cmi_b_createSubItem.Size = new System.Drawing.Size(154, 23);
+            this.cmi_b_createSubItem.TabIndex = 2;
+            this.cmi_b_createSubItem.Text = "Создать под-пункт меню";
+            this.cmi_b_createSubItem.UseVisualStyleBackColor = true;
+            // 
+            // cmi_tb_topItem
+            // 
+            this.cmi_tb_topItem.Location = new System.Drawing.Point(166, 76);
+            this.cmi_tb_topItem.Name = "cmi_tb_topItem";
+            this.cmi_tb_topItem.Size = new System.Drawing.Size(222, 20);
+            this.cmi_tb_topItem.TabIndex = 3;
+            // 
+            // cmi_tb_subItem
+            // 
+            this.cmi_tb_subItem.Location = new System.Drawing.Point(166, 112);
+            this.cmi_tb_subItem.Name = "cmi_tb_subItem";
+            this.cmi_tb_subItem.Size = new System.Drawing.Size(222, 20);
+            this.cmi_tb_subItem.TabIndex = 4;
+            // 
+            // cmi_l_topItem
+            // 
+            this.cmi_l_topItem.AutoSize = true;
+            this.cmi_l_topItem.Location = new System.Drawing.Point(45, 79);
+            this.cmi_l_topItem.Name = "cmi_l_topItem";
+            this.cmi_l_topItem.Size = new System.Drawing.Size(119, 13);
+            this.cmi_l_topItem.TabIndex = 5;
+            this.cmi_l_topItem.Text = "Название пукта меню";
+            // 
+            // cmi_subItem
+            // 
+            this.cmi_subItem.AutoSize = true;
+            this.cmi_subItem.Location = new System.Drawing.Point(24, 115);
+            this.cmi_subItem.Name = "cmi_subItem";
+            this.cmi_subItem.Size = new System.Drawing.Size(140, 13);
+            this.cmi_subItem.TabIndex = 6;
+            this.cmi_subItem.Text = "Название под-пукта меню";
+            // 
+            // cmi_l_currentItem
+            // 
+            this.cmi_l_currentItem.AutoSize = true;
+            this.cmi_l_currentItem.Location = new System.Drawing.Point(50, 43);
+            this.cmi_l_currentItem.Name = "cmi_l_currentItem";
+            this.cmi_l_currentItem.Size = new System.Drawing.Size(114, 13);
+            this.cmi_l_currentItem.TabIndex = 7;
+            this.cmi_l_currentItem.Text = "Текущий пункт меню";
+            // 
+            // cmi_cb_menuItems
+            // 
+            this.cmi_cb_menuItems.FormattingEnabled = true;
+            this.cmi_cb_menuItems.Location = new System.Drawing.Point(166, 38);
+            this.cmi_cb_menuItems.Name = "cmi_cb_menuItems";
+            this.cmi_cb_menuItems.Size = new System.Drawing.Size(222, 21);
+            this.cmi_cb_menuItems.TabIndex = 8;
+            // 
+            // cmi_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 161);
+            this.Controls.Add(this.cmi_cb_menuItems);
+            this.Controls.Add(this.cmi_l_currentItem);
+            this.Controls.Add(this.cmi_subItem);
+            this.Controls.Add(this.cmi_l_topItem);
+            this.Controls.Add(this.cmi_tb_subItem);
+            this.Controls.Add(this.cmi_tb_topItem);
+            this.Controls.Add(this.cmi_b_createSubItem);
+            this.Controls.Add(this.cmi_b_createItem);
+            this.Controls.Add(this.mci_menu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.mci_menu;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "cmi_form";
+            this.Text = "Создание пуктов меню";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mci_menu;
+        private System.Windows.Forms.Button cmi_b_createItem;
+        private System.Windows.Forms.Button cmi_b_createSubItem;
+        private System.Windows.Forms.TextBox cmi_tb_topItem;
+        private System.Windows.Forms.TextBox cmi_tb_subItem;
+        private System.Windows.Forms.Label cmi_l_topItem;
+        private System.Windows.Forms.Label cmi_subItem;
+        private System.Windows.Forms.Label cmi_l_currentItem;
+        private System.Windows.Forms.ComboBox cmi_cb_menuItems;
+    }
+}
+
---
+++ task3/AddMenuItem/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AddMenuItem
+{
+    public partial class cmi_form : Form
+    {
+        int n;
+        ToolStripMenuItem item;
+
+        public cmi_form()
+        {
+            item = null; //объект текущего пукта меню
+            n = -1; //индекс текущего пункта меню
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            cmi_b_createItem.Click += Cmi_b_createItem_Click;
+            cmi_b_createSubItem.Click += Cmi_b_createSubItem_Click;
+            cmi_cb_menuItems.SelectedIndexChanged += Cmi_cb_menuItems_SelectedIndexChanged;
+        }
+
+        /// <summary>
+        /// Обработка изменения выбранного элемента в combo box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Cmi_cb_menuItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (ToolStripMenuItem _item in mci_menu.Items)
+            {
+                if (_item.Text == cmi_cb_menuItems.SelectedItem.ToString())
+                {
+                    item = _item;
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Создание под-пункта меню
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Cmi_b_createSubItem_Click(object sender, EventArgs e)
+        {
+            if (item == null && cmi_cb_menuItems.Items.Count == 0)
+            {
+                MessageBox.Show("Вы не можете добавлять под пункты!\rДобавте - пункты )", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (cmi_tb_subItem.Text.Length > 0)
+                {
+                    if (item == null)
+                    {
+                        foreach (ToolStripMenuItem _item in mci_menu.Items)
+                        {
+                            if (_item.Text == cmi_cb_menuItems.SelectedItem.ToString())
+                            {
+                                item = _item;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (GetSubMenuItemIndex(cmi_tb_subItem.Text, item) == -1)
+                    {
+                        item.DropDownItems.Add(cmi_tb_subItem.Text);
+                        cmi_tb_subItem.Text = "";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Такой под-пункт меню уже существует!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Введите назване под-пункта меню", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Создание пункта меню верхнего уровня
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Cmi_b_createItem_Click(object sender, EventArgs e)
+        {
+            if (cmi_tb_topItem.Text.Length > 0)
+            {
+                item = new ToolStripMenuItem(cmi_tb_topItem.Text);
+
+                if (GetMenuItemIndex(item.Text) == -1)
+                {
+                    mci_menu.Items.Add(item);
+                    cmi_cb_menuItems.Items.Add(cmi_tb_topItem.Text); //добавление в комбо бокс нового пункта меню
+                    cmi_cb_menuItems.SelectedIndex = GetCBIndex(cmi_tb_topItem.Text); //выбор нового названия в комбобоксе
+                    cmi_tb_topItem.Text = "";
+                }
+                else
+                {
+                    item = null;
+                    MessageBox.Show("Такой пункт меню уже существует!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите назване пункта меню", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Получение индекса из меню верхнего уровня
+        /// </summary>
+        /// <param name="title"></param>
+        /// <returns></returns>
+        private int GetMenuItemIndex(string title)
+        {
+            int n = 0;
+            foreach (ToolStripMenuItem item in mci_menu.Items)
+            {
+                if (item.Text == title)
+                {
+                    return n;
+                }
+                n++;
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Получение индекса среди понпуктов выбранного меню
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="_topLevel"></param>
+        /// <returns></returns>
+        private int GetSubMenuItemIndex(string title, ToolStripMenuItem _topLevel)
+        {
+            int n = 0;
+            foreach (ToolStripMenuItem item in _topLevel.DropDownItems)
+            {
+                
+                if (item.Text == title)
+                {
+                    return n;
+                }
+                n++;
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Получение индекса в котейнере комбо бокса
+        /// </summary>
+        /// <param name="title"></param>
+        /// <returns></returns>
+        private int GetCBIndex(string title)
+        {
+            int n = 0;
+            foreach (string item in cmi_cb_menuItems.Items)
+            {
+                if (item == title)
+                {
+                    return n;
+                }
+                n++;
+            }
+            return -1;
+        }
+    }
+}
